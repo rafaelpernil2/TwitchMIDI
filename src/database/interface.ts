8974371cb60ebeb660ffbccbf9add4a644ef6e5d
Rafael Pernil <rafaelpernil@uma.es>
2022-04-22 19:36:43 +0200

    fix: Removed unused files + more documentation

--- src/database/interface.ts
+++ src/database/interface.ts
@@ -1,12 +1,60 @@
 export interface Database<T extends CustomObject<T>> {
+    /**
+     * Retrieves the full DB data
+     */
     get value(): T | undefined;
+
+    /**
+     * Sets a DB
+     */
     set value(v: T | undefined);
+
+    /**
+     * Retrieves all data from path
+     * @param path Path in DB
+     * @returns Data in path
+     */
     selectAll<P extends keyof T>(path: P): T[P] | undefined;
+
+    /**
+     * Retrieves value from path and key
+     * @param path Path in DB
+     * @param key Key in DB
+     * @returns Data in path
+     */
     select<P extends keyof T, S extends keyof T[P]>(path: P, key: S): T[P][S] | undefined;
+
+    /**
+     * Overrides data in DB[path] with a new value
+     * @param path Path in DB
+     * @param value New value to save
+     * @returns ResponseStatus: Ok or Error
+     */
     insertUpdate<P extends keyof T>(path: P, value: T[P]): ResponseStatus;
+
+    /**
+     * Removes data in DB[path][key]
+     * @param path Path in DB
+     * @param key Key in path
+     * @returns ResponseStatus: Ok or Error
+     */
     delete<P extends keyof T>(path: P, key: keyof T[P]): ResponseStatus;
+
+    /**
+     * Saves changes into the persistent DB
+     * @returns Promise of ResponseStatus: Ok or Error
+     */
     commit(): Promise<ResponseStatus>;
+
+    /**
+     * Clears commit data
+     * @returns ResponseStatus: Ok or Error
+     */
     rollback(): ResponseStatus;
+
+    /**
+     * Fetches data from persistent DB asynchronously
+     */
     fetchDB(): Promise<void>;
 }
 
--- src/shared-variable/implementation.ts
+++ src/shared-variable/implementation.ts
@@ -1,14 +1,27 @@
 export class SharedVariable<T> {
     private innerValue: T;
+
+    /**
+     * Given a default value, it creates a SharedVariable instance
+     * @param value Initial value
+     */
     constructor(value: T) {
         this.innerValue = value;
     }
 
+    /**
+     * Retrieves the current value saved in this instance
+     * @returns Value
+     */
     public get(): T {
         return this.innerValue;
     }
 
-    public set(v: T) {
+    /**
+     * Sets a new value to be saved in this instance
+     * @param v Value
+     */
+    public set(v: T): void {
         this.innerValue = v;
     }
 }
